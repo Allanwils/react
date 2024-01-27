a490cc180b92d1f4623dc6176f9657bcd0db8a33
Allan Masiga <zionalehebrew@gmail.com>
2024-01-27 20:19:41 +0300

    task 3 Deploy to a GitHub page

--- 0x00-react_introduction/task_3/dashboard/src/App.js
+++ 0x00-react_introduction/task_3/dashboard/src/App.js
@@ -1,13 +1,14 @@
 import logo from './holberton-logo.jpg';
 import './App.css';
 import { getFooterCopy, getFullYear } from './utils';
-
+import Notifications from './Notifications';
 
 function App() {
   const currentYear = getFullYear();
   const footerCopy = getFooterCopy(true);
   return (
     <div className="App">
+      <Notifications />
       <div className="App-header">
         <img src={logo} className="App-logo" alt=" Logo" />
         <h1>School dashboard</h1>
@@ -15,14 +16,20 @@ function App() {
       <div className="App-body">
         <p>Login to access the full dashboard</p>
         <form>
-        <label htmlFor="Email">
-          Email:
-          <input type="email" name="email" id="email" />
-        </label>
-        <label htmlFor="Password">Password:
-          <input type="password" name="password" id="password" />
-        </label>
-        <button type='submit'>OK</button>
+          <label htmlFor="email">
+            Email:
+            <input type="email" name="email" id="email" autoComplete="email" />
+          </label>
+          <label htmlFor="password">
+            Password:
+            <input
+              type="password"
+              name="password"
+              id="password"
+              autoComplete="current-password"
+            />
+          </label>
+          <button type="submit">OK</button>
         </form>
       </div>
       <div className="App-footer">
--- 0x00-react_introduction/task_3/dashboard/src/index.js
+++ 0x00-react_introduction/task_3/dashboard/src/index.js
@@ -4,10 +4,10 @@ import Notifications from './Notifications';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root.notifications'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Notifications />
+    <App />
   </React.StrictMode>
 );
 
