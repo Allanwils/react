68649e43155c84db56116116a2748af435fc9d2d
Allan Masiga <zionalehebrew@gmail.com>
2024-01-27 11:00:56 +0300

    task 2 Modified the Notifications

--- 0x00-react_introduction/task_2/dashboard/src/index.js
+++ 0x00-react_introduction/task_2/dashboard/src/index.js
@@ -4,7 +4,7 @@ import Notifications from './Notifications';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root.notifications'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
