507b633d822dc9df5161ab4e9975708187e6ff4d
Allan Masiga <zionalehebrew@gmail.com>
2024-01-31 13:06:51 +0300

    task_0 Basic components.

--- 0x02-react_props/task_0/dashboard/src/App/App.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './holberton-logo.jpg';
+import logo from '../holberton-logo.jpg';
 import './App.css';
-import { getFooterCopy, getFullYear } from './utils';
-import Notifications from './Notifications';
-import Login from './Login/Login';
-import Footer from './Footer/Footer';
-import Header from './Header/Header';
+import { getFooterCopy, getFullYear } from '../utils';
+import Notifications from '../Notifications';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
 
 function App() {
   const currentYear = getFullYear();
